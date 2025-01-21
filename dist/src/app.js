bcd46b89eb1eb21e8321c9f766a560a8e5361eb9
husain <hbarwahwala5253@gmail.com>
2025-01-21 13:10:30 +0530

    remove mongo

--- dist/src/app.js
+++ dist/src/app.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata"); // Make sure this is at the top
 const microframework_w3tec_1 = require("microframework-w3tec");
 const expressLoader_1 = require("./loaders/expressLoader");
-const mongoLoader_1 = require("./loaders/mongoLoader");
+// import { mongoLoader } from "./loaders/mongoLoader";
 require("reflect-metadata");
 (0, microframework_w3tec_1.bootstrapMicroframework)([
     expressLoader_1.expressLoader
 ]).then(() => {
-    (0, mongoLoader_1.mongoLoader)();
+    // mongoLoader();
     console.info('connected on port 3000');
 }).catch((err) => {
     console.error(err);
--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import 'reflect-metadata'; // Make sure this is at the top
 
 import { bootstrapMicroframework } from "microframework-w3tec";
 import { expressLoader } from "./loaders/expressLoader";
-import { mongoLoader } from "./loaders/mongoLoader";
+// import { mongoLoader } from "./loaders/mongoLoader";
 
 import 'reflect-metadata';
 
 bootstrapMicroframework([
     expressLoader
 ]).then(() => {
-    mongoLoader();
+    // mongoLoader();
     console.info('connected on port 3000')
 }).catch((err) => {
     console.error(err)
